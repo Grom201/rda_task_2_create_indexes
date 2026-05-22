e5ef127d8fbec3b67ade486fde503b896131d00c
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-05-22 10:34:47 +0200

    Reanmed indexes for database tables

--- task.sql
+++ task.sql
@@ -1,13 +1,13 @@
 USE ShopDB;
 
-CREATE INDEX idx_Email ON Customers (Email);
-CREATE INDEX idx_FirstName ON Customers (FirstName);
-CREATE INDEX idx_LastName ON Customers (LastName);
-CREATE INDEX idx_Name On Products (Name);
-CREATE INDEX idx_Price On Products (Price);
-CREATE INDEX idx_Amount On Products (WarehouseAmount);
-CREATE INDEX idx_Date ON Orders (Date);
-CREATE INDEX idx_CustomerID on Orders (CustomerID);
-CREATE INDEX idx_OrderID ON OrderItems (OrderID);
-CREATE INDEX idx_ProductID ON OrderItems (ProductID);
+CREATE INDEX Email ON Customers (Email);
+CREATE INDEX FirstName ON Customers (FirstName);
+CREATE INDEX LastName ON Customers (LastName);
+CREATE INDEX Name On Products (Name);
+CREATE INDEX Price On Products (Price);
+CREATE INDEX Amount On Products (WarehouseAmount);
+CREATE INDEX Date ON Orders (Date);
+CREATE INDEX CustomerID on Orders (CustomerID);
+CREATE INDEX OrderID ON OrderItems (OrderID);
+CREATE INDEX ProductID ON OrderItems (ProductID);
 
