d56f50082be87ab00edc7addd6ba29c730d5f649
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-05-22 10:31:05 +0200

    Created 10 indexes for database tables

--- task.sql
+++ task.sql
@@ -1,2 +1,13 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+CREATE INDEX idx_Email ON Customers (Email);
+CREATE INDEX idx_FirstName ON Customers (FirstName);
+CREATE INDEX idx_LastName ON Customers (LastName);
+CREATE INDEX idx_Name On Products (Name);
+CREATE INDEX idx_Price On Products (Price);
+CREATE INDEX idx_Amount On Products (WarehouseAmount);
+CREATE INDEX idx_Date ON Orders (Date);
+CREATE INDEX idx_CustomerID on Orders (CustomerID);
+CREATE INDEX idx_OrderID ON OrderItems (OrderID);
+CREATE INDEX idx_ProductID ON OrderItems (ProductID);
+
