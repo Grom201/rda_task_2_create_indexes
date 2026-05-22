2aeb8f3b30464ea09ba41964423c70cbd6b09d41
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-05-22 10:55:05 +0200

    2 indexes keept

--- task.sql
+++ task.sql
@@ -1,12 +1,4 @@
 USE ShopDB;
 
 CREATE INDEX Email ON Customers (Email);
-CREATE INDEX FirstName ON Customers (FirstName);
-CREATE INDEX LastName ON Customers (LastName);
-CREATE INDEX Name On Products (Name);
-CREATE INDEX Price On Products (Price);
-CREATE INDEX WarehouseAmount On Products (WarehouseAmount);
-CREATE INDEX Date ON Orders (Date);
-CREATE INDEX CustomerID on Orders (CustomerID);
-CREATE INDEX OrderID ON OrderItems (OrderID);
-
+CREATE INDEX Name ON Products (Name);
