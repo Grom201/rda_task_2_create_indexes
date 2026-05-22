873ded5f3d727bfa02ed40951587cc6c5315d507
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-05-22 10:49:49 +0200

    renamed amount index to warehouseAmount for products table

--- task.sql
+++ task.sql
@@ -5,7 +5,7 @@ CREATE INDEX FirstName ON Customers (FirstName);
 CREATE INDEX LastName ON Customers (LastName);
 CREATE INDEX Name On Products (Name);
 CREATE INDEX Price On Products (Price);
-CREATE INDEX Amount On Products (WarehouseAmount);
+CREATE INDEX WarehouseAmount On Products (WarehouseAmount);
 CREATE INDEX Date ON Orders (Date);
 CREATE INDEX CustomerID on Orders (CustomerID);
 CREATE INDEX OrderID ON OrderItems (OrderID);
